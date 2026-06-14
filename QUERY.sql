0bb5e63d5cf8e5a888001a01c88d5a86c1b4df49
mdrakibulhasanparvej <mdrakibulhasanparvej@gmail.com>
2026-06-14 18:18:43 +0600

    create matches table

--- QUERY.sql
+++ QUERY.sql
@@ -9,3 +9,13 @@ CREATE TABLE Users(
 );
 
 
+-- 2. CREATE MATCHES TABLE
+ 
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(200) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL(10,2) NOT NULL CHECK(base_ticket_price >= 0),
+    match_status VARCHAR(50) NOT NULL CHECK(match_status IN('Available','Selling Fast','Sold Out','Postponed'))
+);
+
