e7e9728caaf5bf5ebf1a0ea1480de5178c946e0e
mdrakibulhasanparvej <mdrakibulhasanparvej@gmail.com>
2026-06-14 18:28:35 +0600

    All users and their bookings including users with no bookings(Left JOIN)

--- QUERY.sql
+++ QUERY.sql
@@ -91,3 +91,12 @@ FROM Bookings b
 INNER JOIN Users u ON b.user_id = u.user_id
 INNER JOIN Matches m ON b.match_id = m.match_id;
 
+-- Query 5: All users and their bookings, including users with no bookings (LEFT JOIN)
+
+SELECT 
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
+
